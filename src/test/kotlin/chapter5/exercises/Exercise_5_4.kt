9085c119ac30beba2eab56e9fef697b55d78b2ec
Vikas Yadawad <vikasyadawad@gmail.com>
2020-05-16 20:51:19 +0200

    solution to Exercise 5.4

--- src/test/kotlin/chapter5/exercises/Exercise_5_4.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_4.kt
@@ -1,27 +1,29 @@
 package chapter5.exercises
 
+import chapter5.Boilerplate.foldRight
 import chapter5.Stream
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
 //tag::init[]
-fun <A> Stream<A>.forAll(p: (A) -> Boolean): Boolean = TODO()
+fun <A> Stream<A>.forAll(p: (A) -> Boolean): Boolean =
+    foldRight({ true }, { a, b -> p(a) && b() })
 //end::init[]
 
 /**
- * Re-enable the tests by removing the `!` prefix!
+ * Re-enable the tests by removing the `` prefix
  */
 class Exercise_5_4 : WordSpec({
 
     "Stream.forAll" should {
-        "!ensure that all elements match the predicate" {
+        "ensure that all elements match the predicate" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.forAll { it < 6 } shouldBe true
         }
-        """!stop evaluating if one element does not satisfy
+        """stop evaluating if one element does not satisfy
             the predicate""" {
-                val s = Stream.of(1, 2, 3, 4, 5)
-                s.forAll { it != 3 } shouldBe false
-            }
+            val s = Stream.of(1, 2, 3, 4, 5)
+            s.forAll { it == 3 } shouldBe false
+        }
     }
 })
