e01935aad310efb4178198c5a5a0ace810eea84c
Vikas Yadawad <vikasyadawad@gmail.com>
2020-05-16 17:51:23 +0200

    solution to Exercise 5.1

--- src/test/kotlin/chapter5/exercises/Exercise_5_1.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_1.kt
@@ -1,6 +1,10 @@
 package chapter5.exercises
 
 import chapter3.List
+import chapter3.Nil
+import chapter3.exercises.reverse
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
@@ -10,11 +14,24 @@ import io.kotlintest.specs.WordSpec
  */
 class Exercise_5_1 : WordSpec({
     //tag::init[]
-    fun <A> Stream<A>.toList(): List<A> = TODO()
+    fun <A> Stream<A>.toList(): List<A> =
+            when(this) {
+                is Empty -> Nil
+                is Cons -> chapter3.Cons(this.h(), this.t().toList())
+            }
+
+    fun <A> Stream<A>.safeToList(): List<A> {
+        tailrec fun go(stream: Stream<A>, acc: List<A>): List<A> =
+            when(stream) {
+                is Empty -> acc
+                is Cons -> go(stream.t(), chapter3.Cons(stream.h(), acc))
+            }
+        return reverse(go(this, Nil))
+    }
     //end::init[]
 
     "Stream.toList" should {
-        "!force the stream into an evaluated list" {
+        "force the stream into an evaluated list" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.toList() shouldBe List.of(1, 2, 3, 4, 5)
         }
