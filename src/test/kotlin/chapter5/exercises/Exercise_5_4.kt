6f2328d12048ee04dc434971f5cdb198c99e0439
Vikas Yadawad <vikasyadawad@gmail.com>
2020-05-16 21:21:26 +0200

    solution to 5.4

--- src/test/kotlin/chapter5/exercises/Exercise_5_4.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_4.kt
@@ -1,6 +1,8 @@
 package chapter5.exercises
 
 import chapter5.Boilerplate.foldRight
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
@@ -9,6 +11,12 @@ import io.kotlintest.specs.WordSpec
 fun <A> Stream<A>.forAll(p: (A) -> Boolean): Boolean =
     foldRight({ true }, { a, b -> p(a) && b() })
 //end::init[]
+
+fun <A> Stream<A>.forAll2(p: (A) -> Boolean): Boolean =
+    when (this) {
+        is Empty -> true
+        is Cons -> p(this.h()) && this.forAll(p)
+    }
 
 /**
  * Re-enable the tests by removing the `` prefix
@@ -19,11 +27,13 @@ class Exercise_5_4 : WordSpec({
         "ensure that all elements match the predicate" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.forAll { it < 6 } shouldBe true
+            s.forAll2 { it < 6 } shouldBe true
         }
         """stop evaluating if one element does not satisfy
             the predicate""" {
             val s = Stream.of(1, 2, 3, 4, 5)
-            s.forAll { it == 3 } shouldBe false
+            s.forAll { it != 3 } shouldBe false
+            s.forAll2 { it != 3 } shouldBe false
         }
     }
 })
