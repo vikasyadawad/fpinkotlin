3dd7014a9e3a43ffd9be19647993df6d054d8374
Vikas Yadawad <vikasyadawad@gmail.com>
2020-05-16 19:57:04 +0200

    solution to Exercise 5.3

--- src/test/kotlin/chapter5/exercises/Exercise_5_3.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_3.kt
@@ -1,6 +1,8 @@
 package chapter5.exercises
 
 import chapter3.List
+import chapter5.Cons
+import chapter5.Empty
 import chapter5.Stream
 import chapter5.solutions.toList
 import io.kotlintest.shouldBe
@@ -12,24 +14,35 @@ import io.kotlintest.specs.WordSpec
 class Exercise_5_3 : WordSpec({
 
     //tag::init[]
-    fun <A> Stream<A>.takeWhile(p: (A) -> Boolean): Stream<A> = TODO()
+    fun <A> Stream<A>.takeWhile(p: (A) -> Boolean): Stream<A> {
+        fun go(p: (A) -> Boolean, acc: Stream<A>): Stream<A> =
+                when (acc) {
+                    is Empty -> Stream.empty()
+                    is Cons ->
+                        if (p(acc.h()))
+                            Stream.cons(acc.h, { go(p, acc.t()) })
+                        else
+                            Stream.empty()
+                }
+        return go(p, this)
+    }
     //end::init[]
 
     "Stream.takeWhile" should {
-        "!return elements while the predicate evaluates true" {
+        "return elements while the predicate evaluates true" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.takeWhile { it < 4 }.toList() shouldBe
-                List.of(1, 2, 3)
+                    List.of(1, 2, 3)
         }
-        "!return all elements if predicate always evaluates true" {
+        "return all elements if predicate always evaluates true" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.takeWhile { true }.toList() shouldBe
-                List.of(1, 2, 3, 4, 5)
+                    List.of(1, 2, 3, 4, 5)
         }
-        "!return empty if predicate always evaluates false" {
+        "return empty if predicate always evaluates false" {
             val s = Stream.of(1, 2, 3, 4, 5)
             s.takeWhile { false }.toList() shouldBe
-                List.empty()
+                    List.empty()
         }
     }
 })
