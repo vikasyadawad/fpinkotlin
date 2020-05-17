c8e28df0e402a6b0a231619e07aaca858f733a0c
Vikas Yadawad <vikasyadawad@gmail.com>
2020-05-17 10:48:41 +0200

    solution to 5.11

--- src/test/kotlin/chapter5/exercises/Exercise_5_11.kt
+++ src/test/kotlin/chapter5/exercises/Exercise_5_11.kt
@@ -3,6 +3,8 @@ package chapter5.exercises
 import chapter3.List
 import chapter4.Option
 import chapter4.Some
+import chapter4.exercises.getOrElse
+import chapter4.exercises.map
 import chapter5.Stream
 import chapter5.solutions.take
 import chapter5.solutions.toList
@@ -10,7 +12,9 @@ import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 
 //tag::init[]
-fun <A, S> unfold(z: S, f: (S) -> Option<Pair<A, S>>): Stream<A> = TODO()
+fun <A, S> unfold(z: S, f: (S) -> Option<Pair<A, S>>): Stream<A> =
+    f(z).map { Stream.cons({ it.first }) { unfold(it.second, f) } }
+        .getOrElse { Stream.empty() }
 //end::init[]
 
 /**
@@ -18,12 +22,12 @@ fun <A, S> unfold(z: S, f: (S) -> Option<Pair<A, S>>): Stream<A> = TODO()
  */
 class Exercise_5_11 : WordSpec({
     "unfold" should {
-        """!return a stream based on an initial state and a function
+        """return a stream based on an initial state and a function
             applied to each subsequent element""" {
-                unfold(0, { s: Int ->
-                    Some(Pair(s, s + 1))
-                }).take(5).toList() shouldBe
-                    List.of(0, 1, 2, 3, 4)
-            }
+            unfold(0, { s: Int ->
+                Some(Pair(s, s + 1))
+            }).take(5).toList() shouldBe
+                List.of(0, 1, 2, 3, 4)
+        }
     }
 })
